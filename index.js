92f857bb8150f9a204a7cf3e83eac7de20819cea
nehakgg <107543046+nehakgg@users.noreply.github.com>
2023-12-25 13:58:51 +0530

    seach alumuni udate

--- index.js
+++ index.js
@@ -53,8 +53,11 @@ import mongoose from 'mongoose';
 import morgan from 'morgan';
 import cors from 'cors';
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
 
 app.use(morgan('tiny'));
 app.use(express.json());
@@ -70,14 +73,25 @@ mongoose.connect('mongodb://localhost:27017/ryan', {
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
+  roll: String,
+  email: String,
+  graduationYear: String,
+  branch: String,
 });
+
+
+
+
 
 // Create a User model based on the schema
 const User = mongoose.model('User', userSchema);
 
+
+
+
 // Route handler for signup
 app.post('/signup', async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, roll, email, graduationYear, branch } = req.body;
 
   try {
     // Check if the user already exists in the database
@@ -87,7 +101,14 @@ app.post('/signup', async (req, res) => {
     }
 
     // Create a new user and save it to the database
-    const newUser = new User({ username, password });
+    const newUser = new User({
+      username,
+      password,
+      roll,
+      email,
+      graduationYear,
+      branch,
+    });
     await newUser.save();
 
     res.status(201).json({ message: 'User created successfully' });
@@ -116,6 +137,89 @@ app.post('/login', async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
+
+
+// Define a user schema
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+
+
+
+
+// Create a User model based on the schema
+const  admin = mongoose.model('admin', adminSchema);
+
+
+app.post('/AdminSignup', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if the user already exists in the database
+    const existingUser = await admin.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already exists' });
+    }
+
+    // Create a new user and save it to the database
+    const newUser = new admin({ username, password });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+app.post('/AdminLogin', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if the user exists in the database
+    const existingadmin = await admin.findOne({ username, password });
+    if (existingadmin) {
+      res.status(200).json({ message: 'Login successful' });
+      console.log("Successful login");
+    } else {
+      res.status(401).json({ message: 'Login failed' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+app.get('/search/:username', async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    // Check if the user exists in the database
+    const user = await User.findOne({ username });
+
+    if (user) {
+      res.status(200).json({
+
+        username: user.username,
+        roll: user.roll,
+        email: user.email,    // Add email field
+        branch: user.branch,
+        graduationYear: user.graduationYear
+        // Add other user details you want to send to the client
+      });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 
 
 // Start the server

394c675e7705510f3fd010a866737f12851fbcc4
nehakgg <107543046+nehakgg@users.noreply.github.com>
2023-12-28 01:26:16 +0530

    added client react

--- index.js
+++ index.js
@@ -222,6 +222,38 @@ app.get('/search/:username', async (req, res) => {
 
 
 
+// Simple in-memory database for demonstration purposes
+
+
+// Change Password API endpoint
+// ... (previous code)
+
+// Change Password API endpoint
+app.post('/change-password', async (req, res) => {
+  const { username, currentPassword, newPassword } = req.body;
+
+  try {
+    // Check if the user exists in the database
+    const user = await User.findOne({ username, password: currentPassword });
+    
+    if (user) {
+      // Update the password
+      user.password = newPassword;
+      await user.save();
+
+      return res.status(200).json({ message: 'Password changed successfully' });
+    } else {
+      return res.status(401).json({ error: 'Current password is incorrect' });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// ... (remaining code)
+
+
 // Start the server
 app.listen(5000, () => {
   console.log(`Server is running on port ${PORT}`);
